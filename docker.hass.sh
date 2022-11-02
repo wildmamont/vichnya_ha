e1a5c0438ab7c662f73c48cafc64c0a53ba6c754
Yakov Borisov <yakov_borisov@mail.ru>
2022-11-03 01:02:47 +0300

    update mqtt

--- docker.hass.sh
+++ docker.hass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --init --restart unless-stopped -d --name="home-assistant" -e "TZ=Europe/Moscow" -v /home/homeassistant/.homeassistant:/config  --net=host homeassistant/raspberrypi4-homeassistant:stable
--v /sys/devices/w1_bus_master1:/sys/devices/w1_bus_master1  
--v /sys/bus/w1:/sys/bus/w1 
+docker run --restart unless-stopped -d --name="home-assistant" -e "TZ=Europe/Moscow" -v /home/homeassistant/.homeassistant:/config  --net=host homeassistant/raspberrypi4-homeassistant:stable
+#-v /sys/devices/w1_bus_master1:/sys/devices/w1_bus_master1  
+#-v /sys/bus/w1:/sys/bus/w1 
